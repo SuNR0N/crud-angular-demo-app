d4af308aeb439c7b17264992deba5574ed293f0f
Norbert Annus <Norbert.Annus@wipro.com>
2018-08-06 12:53:29 +0100

    Add icon generation, wire in ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -12,6 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [ AppComponent ]
